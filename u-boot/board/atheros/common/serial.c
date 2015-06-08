f85602e38e542c4eca3809df6de8f9150e186b0f
Mantas Pucka <mantas@8devices.com>
2015-06-08 13:20:14 +0300

    Add support for baudrate change for qca953x Remove unrelated GPIO init from serial driver

--- u-boot/board/atheros/common/serial.c
+++ u-boot/board/atheros/common/serial.c
@@ -18,32 +18,65 @@
 #include <asm/addrspace.h>
 #include <asm/types.h>
 #include <config.h>
+#include <common.h>
 #include <atheros.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+/*
+ * Divide positive or negative dividend by positive divisor and round
+ * to closest integer. Result is undefined for negative divisors and
+ * for negative dividends if the divisor variable type is unsigned.
+ */
+#define DIV_ROUND_CLOSEST(x, divisor)(			\
+{							\
+	typeof(x) __x = x;				\
+	typeof(divisor) __d = divisor;			\
+	(((typeof(x))-1) > 0 ||				\
+	 ((typeof(divisor))-1) > 0 || (__x) > 0) ?	\
+		(((__x) + ((__d) / 2)) / (__d)) :	\
+		(((__x) - ((__d) / 2)) / (__d));	\
+}							\
+)
 
 int serial_init(void)
 {
-//#if !defined(CONFIG_ATH_EMULATION)
-	uint32_t div, val;
-
-	div = ath_uart_freq() / (16 * CONFIG_BAUDRATE);
-#if defined(CONFIG_SCO_SLAVE_CONNECTED)
-	val = ath_reg_rd(GPIO_OE_ADDRESS) & (~0xcbf410u);
-#elif defined(CONFIG_MACH_QCA956x)
-	val = ath_reg_rd(GPIO_OE_ADDRESS) & 0xbbfdf6;
-#else
-	val = ath_reg_rd(GPIO_OE_ADDRESS) & (~0xcffc10u);
-#endif
-
-#if defined(CONFIG_ATH_SPI_NAND_CS_GPIO)
-	val |= 1 << CONFIG_ATH_SPI_NAND_CS_GPIO;
-#endif
-#if defined(CONFIG_ATH_SPI_CS1_GPIO)
-	val |= 1 << CONFIG_ATH_SPI_CS1_GPIO;
-#endif
-	ath_reg_wr(GPIO_OE_ADDRESS, val);
+	uint32_t val;
 
 #ifdef CONFIG_MACH_QCA956x
+	uart_gpio_init_qca956x();
+#else
+	uart_gpio_init_qca953x();
+#endif
+	serial_setbrg();
 
+	ath_uart_wr(OFS_DATA_FORMAT, 0x3);
+	ath_uart_wr(OFS_INTR_ENABLE, 0);
+
+	return 0;
+}
+
+void uart_gpio_init_qca953x(void)
+{
+	uint32_t val;
+
+	ath_reg_rmw_set(GPIO_OE_ADDRESS, (1<<9));
+	ath_reg_rmw_clear(GPIO_OE_ADDRESS, (1<<10));
+
+	ath_reg_rmw_set(GPIO_OUT_FUNCTION2_ADDRESS,
+			GPIO_OUT_FUNCTION2_ENABLE_GPIO_10_SET(0x16));
+
+	ath_reg_rmw_clear(GPIO_IN_ENABLE0_ADDRESS,
+			GPIO_IN_ENABLE0_UART_SIN_SET(0xff));
+
+	ath_reg_rmw_set(GPIO_IN_ENABLE0_ADDRESS,
+			GPIO_IN_ENABLE0_UART_SIN_SET(0x9));
+}
+
+
+void uart_gpio_init_qca956x(void)
+{
+#ifdef CONFIG_MACH_QCA956x
 #if defined(UART_RX20_TX22)
 
 	val = (ath_reg_rd(GPIO_OE_ADDRESS) & (~0x400000));
@@ -147,44 +180,7 @@ int serial_init(void)
 
 	val = ath_reg_rd(GPIO_SPARE_ADDRESS);
 	ath_reg_wr(GPIO_SPARE_ADDRESS, (val | 0x8402));
-
-
-#else
-	ath_reg_rmw_set(GPIO_OUT_FUNCTION2_ADDRESS,
-			GPIO_OUT_FUNCTION2_ENABLE_GPIO_10_SET(0x16));
-
-	ath_reg_rmw_clear(GPIO_IN_ENABLE0_ADDRESS,
-			GPIO_IN_ENABLE0_UART_SIN_SET(0xff));
-
-	ath_reg_rmw_set(GPIO_IN_ENABLE0_ADDRESS,
-			GPIO_IN_ENABLE0_UART_SIN_SET(0x9));
-
-	val = ath_reg_rd(GPIO_OUT_ADDRESS) | 0xcffc10u;
-	ath_reg_wr(GPIO_OUT_ADDRESS, val);
-
-	val = ath_reg_rd(GPIO_SPARE_ADDRESS);
-	ath_reg_wr(GPIO_SPARE_ADDRESS, (val | 0x8402));
-
-	ath_reg_wr(GPIO_OUT_ADDRESS, 0x2f);
 #endif
-	/*
-	 * set DIAB bit
-	 */
-	ath_uart_wr(OFS_LINE_CONTROL, 0x80);
-
-	/* set divisor */
-	ath_uart_wr(OFS_DIVISOR_LSB, (div & 0xff));
-	ath_uart_wr(OFS_DIVISOR_MSB, ((div >> 8) & 0xff));
-
-	/* clear DIAB bit*/
-	ath_uart_wr(OFS_LINE_CONTROL, 0x00);
-
-	/* set data format */
-	ath_uart_wr(OFS_DATA_FORMAT, 0x3);
-
-	ath_uart_wr(OFS_INTR_ENABLE, 0);
-//#endif
-	return 0;
 }
 
 int serial_tstc (void)
@@ -192,15 +188,15 @@ int serial_tstc (void)
 	return(ath_uart_rd(OFS_LINE_STATUS) & 0x1);
 }
 
-u8 serial_getc(void)
+int serial_getc(void)
 {
 	while(!serial_tstc());
 
-	return ath_uart_rd(OFS_RCV_BUFFER);
+	return (ath_uart_rd(OFS_RCV_BUFFER) & 0xff);
 }
 
 
-void serial_putc(u8 byte)
+void serial_putc(const char byte)
 {
 	if (byte == '\n') serial_putc ('\r');
 
@@ -211,6 +207,19 @@ void serial_putc(u8 byte)
 
 void serial_setbrg (void)
 {
+	uint32_t div;
+
+	div = DIV_ROUND_CLOSEST(ath_uart_freq(), (16 * gd->baudrate));
+
+	/* set DIAB bit */
+	ath_uart_wr(OFS_LINE_CONTROL, ath_uart_rd(OFS_LINE_CONTROL) | 0x80);
+
+	/* set divisor */
+	ath_uart_wr(OFS_DIVISOR_LSB, (div & 0xff));
+	ath_uart_wr(OFS_DIVISOR_MSB, ((div >> 8) & 0xff));
+
+	/* clear DIAB bit*/
+	ath_uart_wr(OFS_LINE_CONTROL, ath_uart_rd(OFS_LINE_CONTROL) & ~0x80);
 }
 
 void serial_puts (const char *s)
